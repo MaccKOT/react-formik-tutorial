51d72e82950e6c6de0bbe06acc5378fe98c1a27f
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2021-02-01 11:36:28 +0400

    Add validation schema with Yup

--- src/App.js
+++ src/App.js
@@ -16,7 +16,24 @@ class App extends React.Component {
             acceptedTerms: false,
             specialPower: '',
           }}
-          validationSchema={}></Formik>
+          validationSchema={Yup.object({
+            name: Yup.string()
+              .min(3, 'Must be at least 3 charasters')
+              .max(15, 'Must be 15 charasters or less')
+              .required('Required'),
+            email: Yup.string()
+              .email('Invalid email address')
+              .required('Required'),
+            acceptedTerms: Yup.boolean()
+              .required('Required')
+              .oneOf([true], 'You must accept terms'),
+            specialPower: Yup.string()
+              .oneOf(
+                ['flight', 'invicibility', 'wealthy bat guy', 'other'],
+                'Invalid special power'
+              )
+              .required('Required'),
+          })}></Formik>
       </Styles>
     );
   }
